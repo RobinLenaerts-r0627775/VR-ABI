0b7ebe7ce82455afcc2ff572c3b580a8f50d9c8f
RobinLenaerts-r0627775 <robin.lenaerts1@student.ucll.be>
2019-03-28 10:43:01 +0100

    added hologram material to add to the video texture, added a cone for the hologram, added bobbing animation script for the cone

---
+++ ABI-VR/Assets/ABI-VR/Scripts/ScaleBobbing.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleBobbing : MonoBehaviour
+{
+
+    [Header("Bobbing parametres")]
+    public float amplitude;          //Set in Inspector 
+    public float speed;                  //Set in Inspector 
+    private float tempVal;
+    private Vector3 tempPos;
+
+    void Start()
+    {
+        tempVal = transform.localScale.z;
+        tempPos = transform.localScale;
+    }
+
+    void Update()
+    {
+        tempPos.z = tempVal + amplitude * Mathf.Sin(speed * Time.time);
+        transform.localScale = tempPos;
+    }
+}
---
+++ ABI-VR/Assets/ABI-VR/Scripts/VideoControls.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoControls : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TogglePlayPause()
+    {
+        var video = GetComponent<VideoPlayer>();
+        if (video.isPlaying)
+        {
+            video.Pause();
+        }
+        else
+        {
+            video.Play();
+        }
+    }
+}
