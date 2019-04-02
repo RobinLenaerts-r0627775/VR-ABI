18e0a9bb030490c65f6060e6ecf3229ba5d1bc2c
RobinLenaerts-r0627775 <robin.lenaerts1@student.ucll.be>
2019-04-02 13:24:34 +0200

    changed how the menu toggle works so its future proof

--- ABI-VR/Assets/ABI-VR/Scripts/MenuManager.cs
+++ ABI-VR/Assets/ABI-VR/Scripts/MenuManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
@@ -10,13 +11,12 @@ namespace Interactive360
     {
         public static MenuManager instance = null;
         public Button[] m_buttonsInScene; //A reference to all the buttons in the scene that would load new scenes
-        public GameObject m_brands; //A reference to the menu being rendered
-        public GameObject m_categories; //A reference to the button that toggles the video content to play
+        public List<GameObject> m_menuScreens; //A reference to all menu screens 
 
         [SerializeField] string m_oculusMenuToggle = "Button4"; //The name of the oculus button input that will toggle the scene on and off
 
         private AudioSource m_menuToggleAudio; //Audio clip to play when the menu is closed
-
+        public GameObject activemenu;
 
         //make sure that we only have a single instance of the game manager
         void Awake()
@@ -31,9 +31,10 @@ namespace Interactive360
                 Destroy(gameObject);
             }
         }
-        // on Start, bind all buttons to their respective scenes and call DontDestroyOnLoad to keep the Main Menu in every scene
+
         void Start()
         {
+            activemenu = m_menuScreens[0];
             m_menuToggleAudio = GetComponent<AudioSource>();
         }
 
@@ -43,28 +44,41 @@ namespace Interactive360
             checkForInput();
         }
 
-        //if the menu is active, turn it off. If it is inactive, turn it on
-        public void toggleMenu()
+        public void addMenuScreen(GameObject screen)
         {
-            var es = (EventSystem)FindObjectOfType(typeof(EventSystem));
-            if (m_brands.activeInHierarchy)
+            m_menuScreens.Add(screen);
+        }
+
+        public void popMenuScreen()
+        {
+            m_menuScreens.Remove(m_menuScreens[m_menuScreens.Count - 1]);
+        }
+
+        //toggle between different menu screens: brands, extra(, voiceover control,...)
+        public void toggleMenu(GameObject screen)
+        {
+            activemenu = screen;
+            foreach (GameObject menu in m_menuScreens)
+            {
+                menu.SetActive(false);
+            }
+
+            if (screen == m_menuScreens[0]) 
+            {
+                m_menuScreens[0].SetActive(true);
+                m_buttonsInScene[0].Select();
+            }
+
+            else if (screen == m_menuScreens[1])
             {
                 //set the categories menu
-                m_brands.SetActive(false);
-                m_categories.SetActive(true);
+                m_menuScreens[1].SetActive(true);
                 m_buttonsInScene[6].Select();
-            }
-            else if (m_categories.activeInHierarchy)
-            {
-                //set the brands menu
-                m_categories.SetActive(false);
-                es.firstSelectedGameObject = null;
 
             }
             else
             {
-                m_brands.SetActive(true);
-                m_buttonsInScene[0].Select();
+                screen.SetActive(true);
             }
         }
 
@@ -74,17 +88,15 @@ namespace Interactive360
             //check for input from specified Oculus Touch button or the App button on Google Daydream Controller
             if (Input.GetButtonDown(m_oculusMenuToggle))
             {
-                toggleMenu();
+                int menuindex = m_menuScreens.IndexOf(activemenu) +1;
+                if (menuindex >= m_menuScreens.Count) menuindex -= m_menuScreens.Count;
+                toggleMenu(m_menuScreens[menuindex]);
 
                 //if we have an audio source to play with menu toggle, play it now
                 if (m_menuToggleAudio)
                     m_menuToggleAudio.Play();
             }
-
         }
     }
-        
-    
-
 }
 
