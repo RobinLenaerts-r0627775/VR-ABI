eb36b2eaff5caa998ea1378b6f13ee061dad05d0
hermanscedric <cedric.hermans@student.ucll.be>
2019-04-09 11:55:25 +0200

    buttons generated

--- ABI-VR/Assets/ABI-VR/Scripts/CMSLoader.cs
+++ ABI-VR/Assets/ABI-VR/Scripts/CMSLoader.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -18,45 +19,37 @@ public class CMSLoader : MonoBehaviour
         // GET CANVAS
         var canvas = GameObject.FindWithTag("Canvas");
 
-        // CREATE BUTTONS USING CUBES
-        for (int i = 0; i <= dictionary.Count - 1; i++)
-        {
-            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            cube.transform.localPosition = new Vector3(-500, -500, -500);
-            cube.transform.localScale = new Vector2(1000, 1000);
-        }
-
-        // LOOP IMAGES
+        // CREATE BUTTON
         for (int i = 0; i <= dictionary.Count - 1; i++)
         {
             string code = dictionary.Keys.ElementAt(i);
             string file = dictionary.Values.ElementAt(i);
-
             Texture2D texture = LoadImage(file);
             WWW www = new WWW(file);
             yield return www;
             www.LoadImageIntoTexture(texture);
-            var go = GameObject.FindWithTag("Image");
-            var image = go.GetComponent<RawImage>();
-
-            if (texture.width >= 1600)
-            {
-                image.transform.localScale = new Vector2(texture.width / 200, texture.height / 200);
-            }
-            else
-            {
-                image.transform.localScale = new Vector2(texture.width / 100, texture.height / 100);
-            }
-
-            image.texture = texture;
-
-            // CODE
-            var codeGO = (GameObject)GameObject.FindWithTag("Code");
-            var codeComp = codeGO.GetComponent<Text>();
-            codeComp.text = "Code : " + code;
-
-            yield return new WaitForSecondsRealtime(2.0f);
+            GameObject gob = new GameObject("Button");
+            gob.transform.localPosition = new Vector3(100 * i, 0, 0);
+            GameObject tob = new GameObject("Text");
+            gob.transform.SetParent(canvas.transform, false);
+            tob.transform.SetParent(gob.transform, false);
+            var button = gob.AddComponent<Button>();
+            var image = gob.AddComponent<Image>();
+            image.sprite = AssetDatabase.GetBuiltinExtraResource<Sprite>("UI/Skin/UISprite.psd");
+            image.type = Image.Type.Sliced;
+            var text = tob.AddComponent<Text>();
+            text.text = code;
+            text.font = Font.CreateDynamicFontFromOSFont("Arial", 50);
+            text.color = Color.black;
+            text.fontSize = 50;
+            text.alignment = TextAnchor.MiddleCenter;
+            button.onClick.AddListener(() => Test());
         }
+    }
+
+    void Test()
+    {
+        Debug.Log("Joepie");
     }
 
     // Update is called once per frame
