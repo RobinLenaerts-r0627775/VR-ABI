5c43580efc08f32f8b632891be359e756c56eca7
RobinLenaerts-r0627775 <robin.lenaerts1@student.ucll.be>
2019-05-03 16:33:55 +0200

    changed some things about the new video player and used it in the start screen.

---
+++ ABI-VR/Assets/ABI-VR/Scripts/ReturnAfterVideo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class ReturnAfterVideo : MonoBehaviour
+{
+
+    [SerializeField] private VideoPlayer video;
+    [SerializeField] private string JumpTo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        video.loopPointReached += EndReached;
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void EndReached(UnityEngine.Video.VideoPlayer vp)
+    {
+        SceneChanger sc = new SceneChanger();
+        sc.ChangeScene(JumpTo);
+    }
+}
