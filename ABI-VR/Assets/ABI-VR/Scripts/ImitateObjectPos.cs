df82ccc1108cfc5220c7115d28c465614b919c96
Robin Lenaerts <robin_lenaerts2011@hotmail.com>
2019-05-15 10:42:38 +0200

    fixed the hand menu issue

---
+++ ABI-VR/Assets/ABI-VR/Scripts/ImitateObjectPos.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImitateObjectPos : MonoBehaviour
+{
+
+    [SerializeField] private GameObject ImitateObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.localPosition = ImitateObject.transform.localPosition;
+        gameObject.transform.rotation = ImitateObject.transform.rotation;
+    }
+}
