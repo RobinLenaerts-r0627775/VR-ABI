75494e2df575bd830435dd52709896d5a007b268
Robin Lenaerts <robin_lenaerts2011@hotmail.com>
2019-05-16 13:05:17 +0200

    added the Brito video to the main menu

---
+++ ABI-VR/Assets/toggleComponent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toggleComponent : MonoBehaviour
+{
+
+    [SerializeField] private GameObject toToggle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Toggle(){
+        if(toToggle.active) toToggle.SetActive(false);
+        else toToggle.SetActive(true);
+    }
+}
